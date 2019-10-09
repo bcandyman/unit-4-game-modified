1b4c4319d94dfe727834bf1ec90551b97edb0d63
Benjamin Candy <bcandyman93@gmail.com>
2019-10-08 23:22:06 -0700

    Added userScore variable. Added caluculateUserScore function. Added gem button onclick event handlers. Added conditional structure to test game outcome.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,8 @@ var redGem = 0;
 var blueGem = 0;
 var yellowGem = 0;
 var greenGem = 0;
-var targetScore = 0
+var targetScore = 0;
+var userScore = 0;
 
 var gemValues = {
     "red" : 0,
@@ -23,8 +24,41 @@ function randomizeGemValues(min, max){
     }
 }
 
+function calculateUserScore(value){
+    userScore+= value
+    console.log(userScore)
+}
+
 targetScore = getRandomNumberBetween(19,120);
 $("#target").text(targetScore);
+
 randomizeGemValues(1, 12);
 console.log("target score: " + targetScore);
 console.log(gemValues)
+
+if(userScore===targetScore){
+    
+}
+else if(userScore > targetScore){
+
+}
+
+$(".gem-button-red").on("click",function(){
+    console.log("Red Button Clicked!!")
+    calculateUserScore(gemValues.red)
+})
+
+$(".gem-button-blue").on("click",function(){
+    console.log("Blue Button Clicked!!")
+    calculateUserScore(gemValues.blue)
+})
+
+$(".gem-button-yellow").on("click",function(){
+    console.log("Yellow Button Clicked!!")
+    calculateUserScore(gemValues.yellow)
+})
+
+$(".gem-button-green").on("click",function(){
+    console.log("Green Button Clicked!!")
+    calculateUserScore(gemValues.green)
+})
