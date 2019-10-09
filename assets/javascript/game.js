0c39a2d12b5b59f4ce5a25d8ff477154e7a6cd47
Benjamin Candy <bcandyman93@gmail.com>
2019-10-09 00:23:04 -0700

    Removed unused variables. Added conditional code into calculateUserScore function to determine outcome of the game. Added reset function to reset document upon completion of a game round.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,7 @@
-// Declare variables
-var redGem = 0;
-var blueGem = 0;
-var yellowGem = 0;
-var greenGem = 0;
+var loses = 0;
 var targetScore = 0;
 var userScore = 0;
-
+var wins = 0;
 var gemValues = {
     "red" : 0,
     "blue" : 0,
@@ -13,36 +9,59 @@ var gemValues = {
     "green" : 0,
 }
 
+/////////Functions/////////
 
+//This function returns a random number between the min and max parameters
 function getRandomNumberBetween(min, max){
     return Math.floor(Math.random() * (max - min + 1 )) + min;
 }
 
+//This function loops through all key in the gemValues.
+//Each item in gemValues will recieve a random value between the min and max parameters.
 function randomizeGemValues(min, max){
     for (var key in gemValues){
-        gemValues[key] = getRandomNumberBetween(1, 12);
+        gemValues[key] = getRandomNumberBetween(min, max);
     }
 }
 
+//This function adds the value of the gem button pressed to the total score.
+//This function determines if the player wins or loses.
 function calculateUserScore(value){
     userScore+= value
     console.log(userScore)
+    $(".score-total").text(userScore)
+    if(userScore===targetScore){
+        wins++
+        $("#wins").text(wins)
+        reset()
+    }
+    else if(userScore > targetScore){
+        loses++
+        $("#loses").text(loses)
+        reset()
+    }
 }
 
-targetScore = getRandomNumberBetween(19,120);
-$("#target").text(targetScore);
-
-randomizeGemValues(1, 12);
-console.log("target score: " + targetScore);
-console.log(gemValues)
-
-if(userScore===targetScore){
-    
-}
-else if(userScore > targetScore){
-
+//This function reset the document after each round played.
+function reset(){
+    targetScore = getRandomNumberBetween(19,120);
+    randomizeGemValues(1, 12);
+    userScore = 0;
+    $("#target").text(targetScore);
+    $(".score-total").text(0)
 }
 
+
+///////////Main///////////
+//This area is initializing the game upon loading.
+loses = 0;
+wins = 0;
+reset()
+$("#wins").text(wins);
+$("#loses").text(loses);
+
+
+///////////Event Handling//////////
 $(".gem-button-red").on("click",function(){
     console.log("Red Button Clicked!!")
     calculateUserScore(gemValues.red)
